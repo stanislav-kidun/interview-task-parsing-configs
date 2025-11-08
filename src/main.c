4dad310621ef0ae1df725c6dd03edde08755322e
stanislav-kidun <kruzenger@gmail.com>
2025-11-08 18:27:33 +0300

    added simple tests for output functions

--- src/main.c
+++ src/main.c
@@ -1,5 +1,39 @@
+#include <malloc.h>
+
 #include "input_and_output.h"
 
-int main(int argc, char* argv[]) {
+void test_print_with_indent() {
+    open_parentheses();
+    print_with_indent("%s", "mama myla ramu\n");
+    close_parentheses();
+}
 
+void test_print_node_with_single_attr() {
+    struct node node;
+    node.name = "Node1";
+    node.attr = NULL;
+    node.next = NULL;
+    node.child = NULL;
+
+    struct attr attr;
+    attr.name = "Attr1";
+    attr.value = "Value1";
+    attr.next = NULL;
+
+    node.attr = &attr;
+
+    print_config(&node);
+}
+
+int main(int argc, char* argv[]) {
+    // tests
+    test_print_with_indent();
+    test_print_node_with_single_attr();
+
+    struct node* node_ref = read_config_from_file(argc, argv);
+    if (node_ref) {
+        print_config(node_ref);
+    }
+
+    return 0;
 }
