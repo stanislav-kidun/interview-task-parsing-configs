08cfa8d1404d266a561730d0db718e1daa5e862e
stanislav-kidun <kruzenger@gmail.com>
2025-11-08 12:14:32 +0300

    added configs structure

--- src/configs_structure.h
+++ src/configs_structure.h
@@ -1,2 +1,14 @@
 // All config structures should be defined here
 
+struct attr {
+    char* name;
+    char* value;
+    struct attr* next;
+};
+
+struct node {
+    char* name;
+    struct attr* attr;
+    struct node* next;
+    struct node* child;
+};
